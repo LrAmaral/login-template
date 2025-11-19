f76c5e936b08301e4c670becd91ffa3271357a31
LrAmaral <amaralrdev@gmail.com>
2025-11-19 08:18:59 -0300

    feat: add Angular animations dependency and implement routing for login, signup, user, and home pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { User } from './pages/user/user';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
+  },
+  {
+    path: 'user',
+    component: User,
+  },
+  // {
+  //   path: '',
+  //   component: DefaultHomeLayout,
+  //   // canActivateChild: [AuthGuard],
+  //   children: [
+  //     {
+  //       path: 'home',
+  //       component: Home,
+  //     },
+  //   ],
+  // },
+  {
+    path: '**',
+    redirectTo: '/login',
+  },
+];
