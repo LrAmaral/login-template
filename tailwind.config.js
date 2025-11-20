68e95bd2354886e400203068a732c12d950abd49
LrAmaral <amaralrdev@gmail.com>
2025-11-20 11:06:47 -0300

    feat: update asset paths, enhance login layout, and add Tailwind CSS color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7364FF',
+        secondary: '#572AD4',
+        'gray-secondary': '#f1f3f6',
+      },
+    },
   },
   plugins: [],
 };
